41fb3b9b73c9174074176638d55960a10afec645
eunice <atandainioluwa371@gmail.com>
2025-03-19 03:23:01 -0700

    security check

--- app/Shared/firebaseConfig.js
+++ app/Shared/firebaseConfig.js
@@ -7,17 +7,17 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDsVcT58u5fnibQqpN2sX_OEggbFp0bYqs",
-  authDomain: "tubeguruji-app.firebaseapp.com",
-  projectId: "tubeguruji-app",
-  storageBucket: "tubeguruji-app.appspot.com",
-  messagingSenderId: "887202519021",
-  appId: "1:887202519021:web:584d0b453269ff8df979e4",
-  measurementId: "G-GXD1PVGG7Z"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
- const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
  
 export default app;
